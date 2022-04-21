fdd6f6de10c69feabf6891c0ea79f3d79e1db7cc
DiegoAvmor <diegoavmor@gmail.com>
2022-04-21 16:50:45 -0500

    Removed unused routes from the app router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { HomeViewComponent } from "./views/home-view/home-view.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ResourceDetailsView } from "./views/resource-details-view/resource-details-view.component";
-import { AnimesViewComponent } from "./views/animes-view/animes-view.component";
-import { MangasViewComponent } from "./views/mangas-view/mangas-view.component";
 import { ContactViewComponent } from "./views/contact-view/contact-view.component";
 import { SearchViewComponent } from "./views/search-view/search-view.component";
 import { ProfileViewComponent } from "./views/profile-view/profile-view.component";
@@ -12,8 +10,6 @@ import { SignInViewComponent } from "./views/sign-in-view/sign-in-view.component
 
 const routes: Routes = [
   { path: "resource/:type/:id", component: ResourceDetailsView },
-  { path: "animes", component: AnimesViewComponent },
-  { path: "mangas", component: MangasViewComponent },
   { path: "contact", component: ContactViewComponent },
   { path: "search", component: SearchViewComponent },
   { path: "users/:username", component: ProfileViewComponent },
