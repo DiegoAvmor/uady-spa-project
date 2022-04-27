20bb824c7c69b7e5a0189961f57240037f0dbb74
César González <chesarglez429@gmail.com>
2022-04-26 22:08:49 -0500

    :sparkles: Execute the search of first page

--- src/app/views/search-view/search-view.component.ts
+++ src/app/views/search-view/search-view.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { lastValueFrom } from "rxjs";
+import { Anime } from "src/app/models/anime";
+import { Manga } from "src/app/models/manga";
+import { ApiService } from "src/app/services/api.service";
 import { Categories, defaultCategory } from "src/constants";
+
+function sideEffect<Val>(effect: (_: Val) => unknown) {
+  return (value: Val) => {
+    effect(value);
+    return value;
+  };
+}
 
 @Component({
   selector: "app-search-view",
@@ -11,8 +22,15 @@ export class SearchViewComponent implements OnInit {
   category: Categories = defaultCategory;
   query = "";
   Categories = Categories;
+  page = 1;
+  items: Array<Anime | Manga> = [];
+  max_pages = 1;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private apiService: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(({ query, category }) => {
@@ -20,7 +38,24 @@ export class SearchViewComponent implements OnInit {
       if (Object.values(Categories).includes(category)) {
         this.category = category;
       }
+
+      this.searchPage(this.page).then(({ pagination }) => {
+        this.max_pages = pagination.last_visible_page;
+      });
     });
+  }
+
+  searchPage(page: number) {
+    const observable =
+      this.category === Categories.ANIME
+        ? this.apiService.searchAnime(this.query, page)
+        : this.apiService.searchManga(this.query, page);
+
+    return lastValueFrom(observable).then(
+      sideEffect(({ data }) => {
+        this.items = data;
+      })
+    );
   }
 
   changeCategory(category: Categories): void {
