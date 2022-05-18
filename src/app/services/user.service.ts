7ab46093e273738b41eea45dca38577e4fc218b2
DiegoAvmor <diegoavmor@gmail.com>
2022-05-17 21:37:40 -0500

    Removed unused import

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { AuthService } from 'src/app/services/auth.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
