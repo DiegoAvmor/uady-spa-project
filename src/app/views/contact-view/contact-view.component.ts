7be268b55bff39cd07f1338485f85bf9038c5f5b
JohnCaceres <43764825+JohnCaceres@users.noreply.github.com>
2022-05-22 18:13:36 -0500

    Added contact form email functionality

--- src/app/views/contact-view/contact-view.component.ts
+++ src/app/views/contact-view/contact-view.component.ts
@@ -5,7 +5,7 @@ import {
   FormGroup,
   Validators,
 } from "@angular/forms";
-
+import emailjs from '@emailjs/browser';
 @Component({
   selector: "app-contact-view",
   templateUrl: "./contact-view.component.html",
@@ -24,5 +24,21 @@ export class ContactViewComponent implements OnInit {
 
   get form() {
     return this.contactForm.controls;
+  }
+  submitContactForm(): void{
+    emailjs.init("iIkY2dC1ZB5DxdT5_");
+    const templateValues= {
+      from_name: this.form["name"].value,
+      message: this.form["comment"].value,
+      from_email: this.form["email"].value
+
+    };
+    emailjs.send('service_p02zkrj', 'template_96plt0d', templateValues)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
   }
 }
